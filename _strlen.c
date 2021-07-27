43933a8ac585bcd76ef1205b7bafcc7b3130d926
Nonny Jonas <edecjonas@gmail.com>
2021-07-27 14:18:44 -0700

    Added _strlen.c file

---
+++ _strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - a function that gets the length of string
+ * @s: string to get length
+ *
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != 0; len++)
+	{
+	}
+	return (len);
+}
